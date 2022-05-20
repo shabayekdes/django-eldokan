9c97bd2525f68a5420eeadfa3ec92134d9daeee1
Esmail Shabayek <esmail.shabayek@gmail.com>
2022-05-20 23:39:18 +0200

    Change product list  view to generics

--- products/views.py
+++ products/views.py
@@ -1,13 +1,21 @@
 from django.shortcuts import render
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
+from rest_framework import generics
 
 
 from .models import Product
 from .serializers import ProductSerializer
 
-@api_view(['GET'])
-def products_list(request):
-    qs = Product.objects.all()
-    serialize = ProductSerializer(qs, many=True)
-    return Response(serialize.data)
+# @api_view(['GET'])
+# def products_list(request):
+#     qs = Product.objects.all()
+#     serialize = ProductSerializer(qs, many=True)
+#     return Response(serialize.data)
+
+
+class ProductListView(generics.ListCreateAPIView):
+    queryset = Product.objects.all()
+    serializer_class = ProductSerializer
+
+products_list = ProductListView.as_view()
