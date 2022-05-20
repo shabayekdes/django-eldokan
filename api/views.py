4da52229ec96fe4d653eb544f6106f95a30f663f
Esmail Shabayek <esmail.shabayek@gmail.com>
2022-05-20 23:14:29 +0200

    Add home api

--- api/views.py
+++ api/views.py
@@ -1,3 +1,10 @@
-from django.shortcuts import render
+from rest_framework.response import Response 
+from rest_framework.decorators import api_view
 
 # Create your views here.
+@api_view(['GET'])
+def api_home(request):
+    message = {
+        'message': 'Welcome to the API',
+    }
+    return Response(message)
