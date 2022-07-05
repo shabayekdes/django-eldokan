55abf04b7f65a3fa43e7db2589cfc6e220b72f0a
Esmail Shabayek <esmail.shabayek@gmail.com>
2022-07-05 10:41:46 +0200

    Change columns of brand

--- brands/models.py
+++ brands/models.py
@@ -2,13 +2,13 @@ from django.db import models
 
 # Create your models here.
 class Brand(models.Model):
-    name = models.CharField(max_length=255)
+    name = models.CharField(max_length=50)
+    name_ar = models.CharField(max_length=50, blank=True, null=True)
+    image = models.CharField(max_length=255, blank=True, null=True)
+    featured = models.BooleanField()
+    active = models.BooleanField()
     created_at = models.DateTimeField(blank=True, null=True)
     updated_at = models.DateTimeField(blank=True, null=True)
-    name_ar = models.CharField(max_length=255, blank=True, null=True)
-    image = models.CharField(max_length=255, blank=True, null=True)
-    featured = models.IntegerField()
-    active = models.IntegerField()
 
     class Meta:
         db_table = 'brands'
