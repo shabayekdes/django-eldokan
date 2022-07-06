46da4b944da570fd40e0897683c64b6405f7c7dd
Esmail Shabayek <esmail.shabayek@gmail.com>
2022-07-06 12:02:54 +0200

    Hide variants product and transfer to inline main product in admin panel

--- products/admin.py
+++ products/admin.py
@@ -1,5 +1,18 @@
 from django.contrib import admin
 from .models import Product
 
+
 # Register your models here.
-admin.site.register(Product)
+class VariantProductInline(admin.StackedInline):
+    model = Product
+
+
+class ProductAdmin(admin.ModelAdmin):
+    inlines = [VariantProductInline]
+
+    def get_queryset(self, request):
+        qs = super().get_queryset(request)
+        return qs.exclude(parent__isnull=False)
+
+
+admin.site.register(Product, ProductAdmin)
